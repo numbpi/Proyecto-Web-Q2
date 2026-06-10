bd33b74f749eb6acdcf6a79190e25d0b42361268
numbpi <rimv715@gmail.com>
2026-06-10 10:36:06 -0600

    feat(core): Terminando el Modulo de Dashboard
    - Agregando el Dto
    - Agregando el Service
    - Agregando el Controller
    
    La idea con este modulo de Dashboard es que nos devuelva el total de las
    colecciones par aun manejo mas practico para la implementación del
    Dashboard en el Frontend.

---
+++ Services/DashboardService.cs
@@ -0,0 +1,49 @@
+﻿using Proyecto_Web_Q2.DTOs;
+
+namespace Proyecto_Web_Q2.Services;
+
+public class DashboardService(FireBaseService fireBase)
+{
+    private readonly FireBaseService _fireBaseService = fireBase;
+
+    public async Task<DashboardDto> GetAsync()
+    {
+        // Para los caso
+        var casesSnapshot = await _fireBaseService.GetCollection("cases").GetSnapshotAsync();
+        var totalCasos = casesSnapshot.Count;
+        var casosPorStatus = casesSnapshot
+            .Documents.GroupBy((d) => d.GetValue<string>("Status"))
+            .ToDictionary(g => g.Key, g => g.Count());
+
+        // Para los Mediadores
+        var mediatorsSnapshot = await _fireBaseService
+            .GetCollection("mediators")
+            .WhereEqualTo("IsActive", true)
+            .GetSnapshotAsync();
+        var totalMediadoresActivos = mediatorsSnapshot.Count;
+
+        // Para los acuerdos
+        var agreementsSnapshot = await _fireBaseService
+            .GetCollection("agreements")
+            .GetSnapshotAsync();
+        var totalAcuerdos = agreementsSnapshot.Count;
+        var acuerdosFormalizadosTotal = agreementsSnapshot.Documents.Count(d =>
+            d.ContainsField("FormalizedAt") && d.GetValue<DateTime?>("FormalizedAt") != null
+        );
+        var acuerdosPendientesTotal = totalAcuerdos - acuerdosFormalizadosTotal;
+
+        // Para los usuarios
+        var usersSnapshot = await _fireBaseService.GetCollection("users").GetSnapshotAsync();
+        var usuariosRegistradosTotal = usersSnapshot.Count;
+
+        return new DashboardDto
+        {
+            TotalCases = totalCasos,
+            CasesByStatus = casosPorStatus,
+            ActiveMediators = totalMediadoresActivos,
+            FormalizedAgreements = acuerdosFormalizadosTotal,
+            PendingAgreements = acuerdosPendientesTotal,
+            TotalUsers = usuariosRegistradosTotal,
+        };
+    }
+}
