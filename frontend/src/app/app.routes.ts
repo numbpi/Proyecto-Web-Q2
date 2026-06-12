54f66ab3a1768ceee6c3fa4e4d943ae29c28f959
numbpi <rimv715@gmail.com>
2026-06-12 12:41:03 -0600

    pruebas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'login',
     component: Login,
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
   },
 ];
