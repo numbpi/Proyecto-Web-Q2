f81f3d949e8dc8bdd0626fc666185be0a4ccfd3c
numbpi <rimv715@gmail.com>
2026-06-15 15:21:49 -0600

    Feat: Terminando la pagina de Home (falta hacer retoques)

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,14 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { UserService } from '../../services/user.service';
 import { CommonModule } from '@angular/common';
 
+const Roles: Record<string, string> = {
+  admin: 'ADMINISTRADOR',
+  mediator: 'MEDIADOR',
+};
+
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, MatCardModule, MatButtonModule, RouterLink],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatChipsModule,
+    MatProgressSpinnerModule,
+    RouterLink,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
   standalone: true,
@@ -22,6 +40,7 @@ export class Home implements OnInit {
     private authService: AuthService,
     private userService: UserService,
     private router: Router,
+    private cdr: ChangeDetectorRef,
   ) {}
 
   // Este metodo es cuando se renderiza las weas de esto, se podria decir que es como un main
@@ -33,9 +52,10 @@ export class Home implements OnInit {
 
     this.userService.getCurrentUser().subscribe({
       next: (res) => {
-        this.userName = res.fullName ?? 'User';
-        this.userRole = res.role ?? 'ciudadano';
+        this.userName = res.fullName.toLocaleUpperCase() ?? 'User';
+        this.userRole = Roles[res.role] ?? 'CIUDADANO';
         this.isLoading = false;
+        this.cdr.detectChanges();
       },
       error: () => this.onLogout(),
     });
