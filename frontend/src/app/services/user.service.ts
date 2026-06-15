024f29afe5ba355aeadbe83d6f20961c6191fe15
numbpi <rimv715@gmail.com>
2026-06-15 01:34:40 -0600

    feat(core): Agregafeat(core): Agregando el Modulo de User tanto en Backend y Frontend
    Backed:
    - Se agrego el service de User, donde vamos a conseguir la info de el
    - Se agrego el endpoint donde vamos a pasar la info
    
    Frontend:
    - Se realizo el service para comunicarnos con el backend y poder
    renderizar en la home
    - Se termino una parte del TS, porque ocupamos el Modulo del backend del
    service para renderizar segun el roll.
    
    TODO: Terminar el landing de la home

---
+++ frontend/src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { ICurrentUser } from '../models/user.model';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({ providedIn: 'root' })
+export class UserService {
+  protected readonly apiURL: string = 'http://localhost:5174/api/User';
+  constructor(
+    private authService: AuthService,
+    private http: HttpClient,
+  ) {}
+
+  getHeaders = (): HttpHeaders => {
+    const token = this.authService.getToken();
+    return new HttpHeaders({ Authorization: `Bearer ${token}` });
+  };
+
+  getCurrentUser = (): Observable<ICurrentUser> =>
+    this.http.get<ICurrentUser>(`${this.apiURL}/me`, { headers: this.getHeaders() });
+}
